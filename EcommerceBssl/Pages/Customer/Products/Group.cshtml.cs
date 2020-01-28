3e7502e2e075622691d7fe92a75efb5b1b4d9182
Adelaja Seun <adelajadapo@gmail.com>
2020-01-28 14:10:13 +0100

    adding of images to cart

--- EcommerceBssl/Pages/Customer/Products/Group.cshtml.cs
+++ EcommerceBssl/Pages/Customer/Products/Group.cshtml.cs
@@ -20,19 +20,24 @@ namespace EcommerceBssl.Pages.Customer.Products
             _context = context;
         }
        
+
         public SubCategory SubCategory { get; set; }
 
-        public async Task OnGetTaskAsync(int? subgroup)
+        public async Task<IActionResult> OnGetAsync(int? subgroup)
         {
             if (subgroup != null)
 	        {
-                SubCategory = await _context.SubCategories.Include (m => m.Products).FirstOrDefaultAsync(n => n.Id == subgroup.Value);
+                SubCategory = await _context.SubCategories.Include(m => m.Products).ThenInclude(l => l.Images).FirstOrDefaultAsync(n => n.Id == subgroup.Value);
 
-               // return Page();
+                foreach (var item in SubCategory.Products)
+                {
+                    item.Images = await _context.Images.Where(m => m.ProductId == item.Id).ToListAsync();
+                }
+                return Page();
 	        }
             else
             {
-               // return NotFound();
+                return NotFound();
             }
             //SubCategory = await _context.SubCategories.FindAsync(subgroup);
 
